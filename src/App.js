d7eb5c42e4bb29e7e7a3aa6fddba1d6d3be573ab
ertantorizkyf <ertanto.rizky@gmail.com>
2020-06-05 19:18:17 +0700

    Import tailwind css to app and modify layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom"
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import Home from "./Views/Home"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="relative pb-10 min-h-screen">
+      <Router>
+
+        <Header />
+
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
